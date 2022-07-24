9684c65fdf9f2e5faacc342f679b7af8e68bf14e
ABCD1234125 <96473494+ABCD1234125@users.noreply.github.com>
2022-07-24 19:23:58 +0530

    MY LEAPYEAR WEBSITE

---
+++ practicalthings/learning/learn.js
@@ -0,0 +1,27 @@
+// console.log(3**3);
+// console.log(2**2);
+// console.log(5**5);
+
+//(3)3 (5)5 (2)2
+
+// console.log(7**7);
+// console.log(9**2)
+
+//EXPONENTIATION OPERATOR
+
+
+// let a = 10;
+// let b = 10;
+
+// let ab = c = 325;
+// console.log(c , ab);
+
+
+// output of a = 10 , b= 5
+
+a=10,b=15
+b=b-a // 15 - 10 = 5
+a=a-b // 
+
+console.log("THE VALU OF A ACCORDING TO CONDITION IS " + a);
+console.log("THE VALU OF b ACCORDING TO CONDITION IS " + b)
